c937099e536b5b972f43bbc6e042d586410b1fc0
michelknowles <michelknowles@gmail.com>
2020-06-04 16:46:59 -0600

    objects

--- objects/practice.js
+++ objects/practice.js
@@ -81,7 +81,7 @@ backPack.firstPocket = 'chapstick';
 */
 
 //Code here
-
+alert(backPack);
 /*
 You probably noticed that it just alerted [object Object].
 Alerting to see the data in your Object doesn't work so well.
@@ -89,7 +89,7 @@ Instead, console.log your whole backPack object and then check out the console.
 */
 
 //Code here
-
+console.log(backPack);
 ////////// PROBLEM 4 //////////
 
 // Do not edit the code below.
