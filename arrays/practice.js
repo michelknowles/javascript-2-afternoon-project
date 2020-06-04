2320a22c43b04ab9a7113f81cb8b9c02bdb078e7
michelknowles <michelknowles@gmail.com>
2020-06-04 15:13:30 -0600

    almost done arrays

--- arrays/practice.js
+++ arrays/practice.js
@@ -19,7 +19,10 @@ var arr = [10,20,30];
 
 //Code Here
 
-
+function first(arr) {
+  return arr[0];
+}
+first();
 
 ////////// PROBLEM 2 //////////
 
@@ -34,8 +37,10 @@ var arr = [40,50,60];
 
 //Code Here
 
-
-
+function last(arr) {
+  return arr[arr.length -1];
+}
+last();
 ////////// PROBLEM 3 //////////
 
 // Do not edit the code below.
@@ -49,7 +54,13 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 
 //Code Here
 
-
+function looper(family){
+  for(let i = 0; i < family.length; i ++) {
+    alert(family[i]);
+  }
+ 
+}
+looper();
 
 ////////// PROBLEM 4 //////////
 
@@ -64,7 +75,12 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 
 //Code Here
 
-
+function reversedLooper(letters) {
+  for(let i = letters.length - 1; i >= 0; i--){
+    alert(letters[i]);
+  }
+}
+reversedLooper();
 
 ////////// PROBLEM 5 //////////
 
@@ -78,6 +94,16 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
+
+function evenFinder(nums) {
+  if(nums % 2 === 0) {
+    return nums;
+  } else {
+    return nums;
+  }
+}
+evenFinder();
+// function evenFinder(nums)
 
 
 
